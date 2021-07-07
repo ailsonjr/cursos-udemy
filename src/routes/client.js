63168f843de36efd291b37099467292a9b3d1719
Ailson Lima Jr <ailsonjr21@gmail.com>
2021-07-07 19:43:43 -0300

    Added view client_new with a form

--- src/routes/client.js
+++ src/routes/client.js
@@ -4,4 +4,6 @@ let router = express.Router();
 router.get('/', (req, res) => res.send('Hello Client :)'));
 router.get('/welcome', (req, res) => res.send('Welcome to Client Page'));
 
+router.get('/new', (req, res) => res.render('client_new'));
+
 module.exports = router;
